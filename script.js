1dc7b561f935c3c1f7d7af97b7272998c93da3ea
sarah-codebug <sbetack@gmail.com>
2019-04-15 17:12:11 -0500

    add lesson

---
+++ script.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  // Write the `summertime` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function summertime() returns: ", summertime())
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `addFive` below. 
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function double(6) returns: ", addFive(10))
+//////////////////////////////////////////////////////////////////////////
+  // Write `double` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function double(6) returns: ", double(6));
+//////////////////////////////////////////////////////////////////////////
+  // Write `greetMe` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function greetMe("Carl") returns: ", greetMe("Carl"));
+//////////////////////////////////////////////////////////////////////////
+  // Write `isItLessThanTen` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function isItLessThanTen(3) returns: ", isItLessThanTen(3));
+// console.log("The function isItLessThanTen(20) returns: ", isItLessThanTen(20));
+//////////////////////////////////////////////////////////////////////////
+  // Write `inHighSchool` below.
+
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function inHighSchool(17) returns: ", inHighSchool(17));
+  // console.log("The function inHighSchool(5) returns: ", inHighSchool(5));
+//////////////////////////////////////////////////////////////////////////
+  // Write `areTheySpeeding` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function areTheySpeeding(60) returns: ", areTheySpeeding(60));
+  // console.log("The function areTheySpeeding(90) returns: ", areTheySpeeding(90));
+//////////////////////////////////////////////////////////////////////////
+  // Write `addTwoNumbers` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function addTwoNumbers(3, 4) returns: ", addTwoNumbers(3, 4));
+//////////////////////////////////////////////////////////////////////////
+  // Write `multiplyTwoNumbers` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function multiplyTwoNumbers(8, 10) returns: ", multiplyTwoNumbers(8, 10));
+//////////////////////////////////////////////////////////////////////////
+})
